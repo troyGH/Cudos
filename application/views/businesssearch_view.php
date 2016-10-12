f435b49928c18ee9b1e1803b3957f54e0e89e3cf
Troy Nguyen <root@AirMac.local>
2016-10-11 17:07:55 -0700

    zoom added to map

--- application/views/businesssearch_view.php
+++ application/views/businesssearch_view.php
@@ -13,6 +13,7 @@
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 37.3352, lng: -121.8811},
       disableDefaultUI: true,
+      zoomControl: true,
       zoom: 5,
     });
 
